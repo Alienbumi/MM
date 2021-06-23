421aa9478ff06fcc19cb1574f7bb89de1fc17024
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-06-23 21:49:50 +0700

    Add files via upload

---
+++ MT04.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=ethash.unmineable.com:3333
+WALLET=MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Mt04#61ev-k59e
+WORKER=Mt04
+
+cd "$(dirname "$0")"
+
+chmod +x ./MT && sudo ./MT -a ethash -o $POOL -u $WALLET.$WORKER $@
