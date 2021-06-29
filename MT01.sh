24e0489fd305b25e7d563bbfd18c35320bffd3c0
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-06-29 10:01:14 +0700

    Tryking

--- MT01.sh
+++ MT01.sh
@@ -2,8 +2,9 @@
 
 POOL=ethash.unmineable.com:3333
 WALLET=MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Mt01#61ev-k59e
-WORKER=Mt01
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 
 cd "$(dirname "$0")"
 
-chmod +x ./MT && sudo ./MT -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./Tryking && sudo ./Tryking -a ethash -o $POOL -u $WALLET.$WORKER -log
+pause $@
