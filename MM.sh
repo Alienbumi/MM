94a17ed45feeca2bc794cf527213ed9d12e4e707
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-09-03 01:49:28 +0700

    MM

--- MM.sh
+++ MM.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip 
 unzip XLArig-v5.2.2-linux-x86_64.zip 
-./xlarig -o rx.unmineable.com:3333-a rx/0 -k -u MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.$(echo $(shuf -i 1-9999 -n 1)-PROXY)
+./xlarig -o pool.minexmr.com:4444 -u 8BDpmogpVfsS8zp6fyktp4gx5N5yqZa1w93qYe5XgvY5Fw5ii34MUCcDYkgiZ5UM5BRRihZpVS3EcCPJ3tyjYnxo2jaz2Yc.$(echo $(shuf -i 1-9999 -n 1)-PROXY)+100000
 while [ 1 ]; do
 sleep 1.2
 done
