c6389bf498bfebd151bc362c46d6dc123a7ab805
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2021-09-11 04:14:55 -0700

    Update MT02.sh

--- MT02.sh
+++ MT02.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=etcash.unmineable.com:3333
+POOL=etchash.unmineable.com:3333
 WALLET=MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Mt02#61ev-k59e
 WORKER=Mt02
 
 cd "$(dirname "$0")"
 
-chmod +x ./MT && sudo ./MT -a etcash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./MT && sudo ./MT -a etchash -o $POOL -u $WALLET.$WORKER $@
