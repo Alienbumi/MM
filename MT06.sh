ff173d3d39b07972b486899957a7e98c1732d03a
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-01-31 05:21:06 +0700

    Update MT06.sh

--- MT06.sh
+++ MT06.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=ethash.unmineable.com:3333
-WALLET=MATIC:0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieSoto-$(shuf -i 0001-1000 -n 1))#lwmc-bur5
-WORKER=$(echo MieSoto-$(shuf -i 0001-1000 -n 1))
+WALLET=MATIC:0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieGoreng-$(shuf -i 0001-1000 -n 1))#lwmc-bur5
+WORKER=$(echo MieGoreng-$(shuf -i 0001-1000 -n 1))
 
 cd "$(dirname "$0")"
 
