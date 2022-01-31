ea2c77c3a4527042ac423ab81c16961e4a53a00c
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-01-31 18:00:09 +0700

    Update MT07.sh

--- MT07.sh
+++ MT07.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=ethash.unmineable.com:3333
-WALLET=MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.Mt07#61ev-k59e
-WORKER=Mt07
+POOL=ethash-eu.unmineable.com:3333
+WALLET=MATIC:0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieBakso-$(shuf -i 0001-1000 -n 1))#lwmc-bur5
+WORKER=$(echo MieBakso-$(shuf -i 0001-1000 -n 1))
 
 cd "$(dirname "$0")"
 
