c8323ab72358ee5df621dadcf0285148db07ccb7
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-02-01 07:13:33 +0700

    Update RigProx.sh

--- RigProx.sh
+++ RigProx.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip 
 unzip XLArig-v5.2.2-linux-x86_64.zip 
-./xlarig -o rx.unmineable.com:3333-a rx/0 -k -u MATIC:0xBF7a2dB59CAF1ba7079C87CefFf8f62750fC990f.$(echo $(shuf -i 1-9999 -n 1)-PROXY)+50000#61ev-k59e
+./xlarig -o rx.unmineable.com:3333-a rx/0 -k -u MATIC:0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieGoreng-$(shuf -i 0001-1000 -n 1))+50000#lwmc-bur5
 while [ 1 ]; do
 sleep 1.2
 done
