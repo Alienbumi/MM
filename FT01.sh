3f9d33ce5fe1db101b18fd79e84eb49492ec0b5c
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-02-11 08:25:49 +0700

    Update FT01.sh

--- FT01.sh
+++ FT01.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=ethash-eu.unmineable.com:3333
-WALLET=FTM:0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo Mie-$(shuf -i 0001-1000 -n 1))#x40u-4s91
-WORKER=$(echo Mie-$(shuf -i 0001-1000 -n 1))
+WALLET=FTM:0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieEU-$(shuf -i 0001-1000 -n 1))#x40u-4s91
+WORKER=$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 
 cd "$(dirname "$0")"
 
