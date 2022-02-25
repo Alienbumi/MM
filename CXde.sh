6c486d05e07141cbd9c7abf7e89452e486db42e1
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-02-25 18:45:22 +0700

    Update CXde.sh

--- CXde.sh
+++ CXde.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=de.conflux.herominers.com:1170
+POOL=stratum+tcp://de.conflux.herominers.com:1170
 WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieDE-$(shuf -i 0001-1000 -n 1))#x40u-4s91
 WORKER=$(echo MieDE-$(shuf -i 0001-1000 -n 1))
 
