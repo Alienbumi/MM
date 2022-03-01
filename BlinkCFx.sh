e5c33a91d9ee239704b765c7c0cee65426a614af
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-03-01 12:47:40 +0700

    Update BlinkCFx.sh

--- BlinkCFx.sh
+++ BlinkCFx.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-ALGO=octopus
 POOL=stratum+tcp://pool.eu.woolypooly.com:3094
-WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieEU-$(shuf -i 0001-1000 -n 1)) --algo octopus
+WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 WORKER=$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 
 cd "$(dirname "$0")"
 
-chmod +x ./MT && sudo ./MT -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./MT && sudo ./MT -a octopus -o $POOL -u $WALLET.$WORKER $@
