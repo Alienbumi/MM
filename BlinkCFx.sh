0eecd4b2fc5fe78cc2e790387a84a01656972ead
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-03-01 12:49:27 +0700

    Update BlinkCFx.sh

--- BlinkCFx.sh
+++ BlinkCFx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=stratum+tcp://pool.eu.woolypooly.com:3094
+POOL=pool.eu.woolypooly.com:3094
 WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 WORKER=$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 
