23091ff48c093d968589851d36b8b61c95d70449
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-03-01 14:15:36 +0700

    Update BlinkCFx.sh

--- BlinkCFx.sh
+++ BlinkCFx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 POOL=cfx.666pool.com:6655
-WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieEU-$(shuf -i 0001-1000 -n 1))
-WORKER=$(echo MieEU-$(shuf -i 0001-1000 -n 1))
+WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo GassNgengg-$(shuf -i 0001-1000 -n 1))
+WORKER=$(echo GassNgengg-$(shuf -i 0001-1000 -n 1))
 
 cd "$(dirname "$0")"
 
