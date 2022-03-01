e6aff0fcdd3cbe03be4f3c1e89daa54a8bc56a42
Alienbumi <73816473+Alienbumi@users.noreply.github.com>
2022-03-01 15:34:11 +0700

    Update BlinkCFox.sh

--- BlinkCFox.sh
+++ BlinkCFox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 POOL=cfx.666pool.com:6655
-WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e.Korex
+WALLET=0xf3ffd073e16a2c69a6343d30e1d4d4e334133b9e
 WORKER=Korex
 
 cd "$(dirname "$0")"
